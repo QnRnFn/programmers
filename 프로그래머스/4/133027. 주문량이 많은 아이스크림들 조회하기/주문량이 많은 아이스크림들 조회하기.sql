104ce974b19212628c07ad256f4a5688229d8317
QnRnFn <101573279+QnRnFn@users.noreply.github.com>
2024-10-14 13:16:09 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,8 +1,8 @@
 -- 코드를 입력하세요
 -- 7월 아이스크림 + 상반기 아이스크림 큰 순서대로 3개의 맛
 -- union 해서 flavor로 그룹하고 더하기
-with cte as (
-select flavor, sum(total_order)
+
+select flavor 
 from (
 SELECT shipment_id, flavor, total_order
 from first_half
@@ -11,9 +11,5 @@ select shipment_id, flavor, total_order
 from july
     ) as un
 group by flavor
-order by 2 desc
-    )
-
-select flavor
-from cte
+order by sum(total_order) desc
 limit 3
