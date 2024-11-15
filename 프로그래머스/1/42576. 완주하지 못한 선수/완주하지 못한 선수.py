f0ae092bb3f7928477fcf6efebdb1a29d47696e2
QnRnFn <101573279+QnRnFn@users.noreply.github.com>
2024-11-15 11:50:14 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 46.90 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,15 +1,19 @@
-#participant의 원소 중 completion에 없는 것 출력
-# 동명이인을 어떻게 처리할지
-# 
-
 def solution(participant, completion):
-    participant.sort() # len : n개
-    completion.sort() # len : n-1개
+    hash_map = {}
     
-    for i in range(len(completion)):
-        if participant[i] != completion[i]:
-            return participant[i]
-        # 정렬하고 p,c 비교했을때 다른 원소가 없다면
-        # p의 마지막 원소가 다른 사람이라는거니까
-    return participant[-1]
-   
+    for i in participant:
+        if i in hash_map:
+            hash_map[i] +=1
+        else:
+            hash_map[i] = 1
+    
+    
+    for i in completion:
+        if i in hash_map:
+            hash_map[i] -=1
+    
+    for i, count in hash_map.items():
+        if count > 0:
+            return i
+        
+        
